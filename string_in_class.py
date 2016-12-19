edcf93e05d54f9442481b1c7a8ebe9c71c8f323c
maku77 <makkuma77@gmail.com>
2016-12-19 13:39:08 +0900

    Use javap's -v option to extract more literals

--- string_in_class.py
+++ string_in_class.py
@@ -12,6 +12,27 @@ import re
 import subprocess
 import sys
 
+
+class LiteralExtractor:
+    def __init__(self):
+        self.PAT = re.compile(r'= String\s+#\d+\s+// (.+)')
+
+    def extract(self, line):
+        """
+        Extract a string literal in the specified line.
+        Returns None if not found.
+        """
+        match = self.PAT.search(line)
+        if match:
+            return match.group(1)
+        return None
+
+    def extract_and_print(self, line):
+        literal = self.extract(line)
+        if literal:
+            print(literal)
+
+
 def command(cmd):
     """ Runs a command and iterates its output. """
     try:
@@ -24,6 +45,7 @@ def command(cmd):
         print('Could not execute command [' + cmd + ']', file=sys.stderr)
         sys.exit(1)
 
+
 def iter_classes_in_jar(jar_name):
     """ Iterates all classes in the specified JAR file. """
     with zipfile.ZipFile(jar_name, 'r') as zf:
@@ -35,21 +57,18 @@ def iter_classes_in_jar(jar_name):
                 continue
             yield classname
 
-def print_literal(line):
-    """ Prints a string literal if the line includes. """
-    match = re.search(r'// String (.+)', line)
-    if match:
-        print(match.group(1))
 
 if __name__ == '__main__':
     inputfile = sys.argv[1]
+    extractor = LiteralExtractor()
+
     if inputfile.endswith('.class'):
-        cmd = 'javap -c {}'.format(inputfile)
+        cmd = 'javap -v {}'.format(inputfile)
         for line in command(cmd):
-            print_literal(line)
-    else:  # Probably it is JAR
+            extractor.extract_and_print(line)
+    else:  # Probably it is a JAR
         for clazz in iter_classes_in_jar(inputfile):
-            cmd = 'javap -cp {} -c {}'.format(inputfile, clazz)
+            cmd = 'javap -cp {} -v {}'.format(inputfile, clazz)
             for line in command(cmd):
-                print_literal(line)
+                extractor.extract_and_print(line)
 
